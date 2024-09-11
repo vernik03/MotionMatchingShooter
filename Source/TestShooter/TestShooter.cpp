d217b4a6d8943dc4dfd7baa2b6209c4749e154da
Nikita Vertelko <vernik03@gmail.com>
2024-09-11 03:24:36 +0300

    Initial Commit

---
+++ Source/TestShooter/TestShooter.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestShooter, "TestShooter" );
+ 
---
+++ Source/TestShooter/TestShooterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestShooterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATestShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATestShooterGameMode();
+};
+
+
+
